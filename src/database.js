11ad59f14032374ef9edbb7042d6d226110fda00
alejodaniel <adf.coronel@yavirac.edu.ec>
2021-01-30 13:46:37 -0500

    servidor

---
+++ src/database.js
@@ -0,0 +1,9 @@
+const mysql = require('mysql');
+const db = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'12345678',
+    database:'geolocalizacion'
+});
+
+module.exports = db
---
+++ src/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express(); // objeto app
+const cors = require('cors');
+const db = require('./database');
+
+app.set('port', process.env.PORT || 4000 );
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/' , (req, res) => {
+    const query = "SELECT * FROM datos";
+    db.query(query, function(error, results, fields){
+      res.json(results)
+    });
+});
+
+app.post('/' , (req, res) => {
+    const obj = req.body
+    const query = "INSERT INTO datos SET ?";
+    db.query(query, obj, function(error, results, fields){
+       res.json(results.insertId)
+    });
+});
+
+app.listen(app.get('port'), () => {
+  console.log("listening")
+});
